89fc5de489ea9d095c2a1487a8257d7bd2f0721f
Dominik Teiml <18601956+hacker-DOM@users.noreply.github.com>
2023-09-16 16:08:17 +0200

    Add woke_tests from fuzz-template and woke_tests/framework from x.

---
+++ woke_tests/tier2/c_hooks.py
@@ -0,0 +1,34 @@
+from .b_helpers import *
+
+class Hooks(Helpers):
+    @override
+    def pre_sequence(s):
+        # s.tokens = []
+
+        for i in range(NUM_TOKENS):
+            decimals = random_int(
+                # TOKEN_MIN_DECIMALS,
+                18,
+                # TOKEN_MAX_DECIMALS,
+                18,
+            )
+            total_supply = NUM_USERS * NUM_TOKENS_EACH_USER * 10**decimals
+            # token: ERC20 = ERC20.deploy(_totalSupply=total_supply, from_=s.paccs[0])
+            # token.label = num_to_letter(i).upper()
+            # print(f'Created token {token.label} with {decimals} decimals and {total_supply} total supply')
+            # s.tokens.append(token)
+            #
+            # for j in range(NUM_USERS):
+            #     _ = token.transfer(s.users[j], NUM_TOKENS_EACH_USER * 10**decimals, from_=s.paccs[0])
+
+    @override
+    def pre_flow(s, flow: Callable[..., None]):
+        with open(csv, 'a') as f:
+            _ = f.write(f'{s.sequence_num},{s.flow_num},{flow.__name__}\n')
+
+    @override
+    def post_sequence(s):
+        ...
+        # s.tokens = None  # pyright: ignore [reportGeneralTypeIssues]
+        # s.factory = None  # pyright: ignore [reportGeneralTypeIssues]
+        # s.pairs = None  # pyright: ignore [reportGeneralTypeIssues]
